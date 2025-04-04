d961fd4b848d508c8e8f859d334dccc2fbdd2795
Samuel Baťo <samo.bato6@gmail.com>
2025-04-04 11:54:53 +0200

    Create db_connection.php

---
+++ db_connection.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""; // žiadne heslo pri XAMPP
+$database = "moja_stranka"; // presne ten názov, ktorý ste použili
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Spojenie zlyhalo: " . $conn->connect_error);
+}
+?>
